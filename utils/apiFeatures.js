9c4168366eb0cf1bf04500ba713dcbec666cb3fd
Roy-adam <roy1991adam@gmail.com>
2021-01-15 18:30:11 +0200

    limiting field is added

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -1,25 +1,34 @@
 class APIFeatures {
-    constructor(query, queryString) {
-        this.query = query;
-        this.queryString = queryString;
-    }
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
 
-    sort() {
-        if (this.queryString.sort) {
-            const sortBy = (this.queryString.sort).split(',').join(' ');
-            this.query = this.query.sort(sortBy);
-        }
-        return this;
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(",").join(" ");
+      this.query = this.query.sort(sortBy);
     }
+    return this;
+  }
 
-    paginate() {
-        const page = this.queryString.page * 1 || 1;
-        const limit = this.queryString.limit * 1 || 10;
-        const skip = (page - 1) * limit;
+  paginate() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 10;
+    const skip = (page - 1) * limit;
 
-        this.query = this.query.skip(skip).limit(limit);
-        return this;
+    this.query = this.query.skip(skip).limit(limit);
+    return this;
+  }
+
+  // Field Limiting ex: -----/user?fields=name,email,address
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(",").join(" ");
+      this.query = this.query.select(fields);
     }
+    return this;
+  }
 }
 
 module.exports = APIFeatures;
